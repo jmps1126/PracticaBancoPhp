6b5b04a6d70d81fbe895de80b80dcc010965e8d6
jmps1126 <jmps1126@gmail.com>
2017-05-05 08:59:52 -0500

    Se realiza commit de los avances en la practica del banco

--- index.php
+++ index.php
@@ -1,78 +1,26 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-	<link rel="stylesheet" href="css/main.css">
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<title>Formulario Credito</title>
-</head>
-<body background="img/pantallaPrincipal.jpg">
-
-	<div class="col-md-4 center-block text-center espacio-arriba quitar-float">
-
-		<h3 class="text-center negrilla">Formulario Solicitud Crédito</h3>
-		<br>
-
-		<form method="post" name="frm" action="resultado.php">
-
-			<div class="form-group">
-				<label>Nombre Completo</label>
-				<input type="text" class="form-control"  name="nombre">
-			</div>
-
-			<div class="form-group">
-				<label>Cédula Ciudadania</label>
-				<input type="number" class="form-control" name="cedula">
-			</div>
-
-			<div class="form-group">
-				<label>Teléfono</label>
-				<input type="number" class="form-control" name="telefono">
-			</div>
-
-			<div class="form-group">
-				<label>Genero</label>
-					<select class="form-control selectpicker" name="genero">
-						<option value="1">Femenino</option>
-						<option value="2">Masculino</option>
-					</select>
-			</div>
-
-			<div class="form-group">
-				<label>Credito</label>
-					<select class="form-control selectpicker" name="credito">
-						<option value="1">Vivienda 1.50</option>
-						<option value="2">Educacion 1.50</option>
-						<option value="3">Vehiculo 1.8</option>
-						<option value="4">Otro</option>
-					</select>
-					<label>En caso de seleccionar otro</label>
-					<input type="number" class="form-control" name="otroCredito">
-			</div>
-
-			<div class="form-group">
-				<label>Valor solicitado</label>
-				<input type="number" class="form-control" name="valor">
-			</div>
-
-			<div class="form-group">
-				<label>Plazo Credito</label>
-					<select class="form-control selectpicker" name="tiempoCredito">
-						<option value="1">Años</option>
-						<option value="2">Meses</option>
-					</select>
-					<br>
-					<input type="number" class="form-control" name="plazo">
-			</div>
-			
-			<div class="form-group">
-				<input type="submit" value="Enviar" class="btn btn-info text-center">
-			</div>
-
-		</form>
-	</div>
-
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<link rel="stylesheet"  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/animate.css">
+		<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+		<title>The Fast Food</title>
+	</head>
+	<body background="img/fondo.jpg">
+		<div class="col-mg-3 center-block quitar-float text-center espacio-arriba animated rotateInUpRight retraso-animate-1">
+			<img src="img/logo_banco_principal.jpg">
+			<h1 class="pacifico grande color-font-green">Digital Banking</h1>
+			<h4>Haz realidad tus sueños con nosotros, ven y realiza un simulador de credito para verificar las posibilidades de credito con nosotros.</h4>
+	
+			<nav>
+				<a href="principal.php" class="espacio-derecha font-open-sans">Simulador Crédito</a>
+			</nav>
+		</div>
+		
+	</body>
 </html>
--- resultado.php
+++ resultado.php
@@ -2,58 +2,124 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<title>Resultado Solicitud</title>
 </head>
-<body>
-	<div class="col-md-10 center-block text-center espacio-arriba quitar-float">
-		<?php 
-	
+<body background="img/fondo.jpg">
+	<div class="animated fadeInDown retraso-animate-1 espacio-arriba">
+	<nav class="padding-largo text-center">
+		<ul class="no-lista">
+			<li class="col-md-4 inline-block"><a href="index.php" class="pacifico ">Digital Banking</a></li>
+			<li class="col-md-4 inline-block"><a href="principa.php" class="pacifico ">Simulador Crédito</a></li>
+			<li class="col-md-4 inline-block"><a href="contacto.php" class="pacifico ">Contacto</a></li>
+		</ul>
+	</nav>
+	<img src="img/logo_banco_principal.jpg" class="col-md-1">    
+	<img src="img/logo_banco_principal.jpg" class="col-md-1 imgstyle">
+
+	<h1 class="text-center negrilla  pacifico">Resultado simulación crédito</h1>
+		
+
+	<?php 
+
 		$nombre = $_POST['nombre'];
 		$cedula = $_POST['cedula'];
-		$telefono = $_POST['telefono'];
 		$genero = $_POST['genero'];
-		$credito = $_POST['credito'];
-		$otroCredito = $_POST['otroCredito'];
+		$salario = $_POST['salario'];
 		$valor = $_POST['valor'];
-		$tiempoCredito = $_POST['tiempoCredito'];
+		$credito = $_POST['credito'];
+		$otroPorcentaje = $_POST['otroPorcentaje'];
 		$plazo = $_POST['plazo'];
-		
-		//determinamos su genero
+
+		$interes =0.0;
+		$cuota =0.0;
+
 		if($genero == 1){
-			$generoFinal = "Señora";
+			$generoFinal = "Señora ";
 		}else{
-			$generoFinal = "Señor";
+			$generoFinal = "Señor ";
 		}
 
 
+		switch ($credito) {
+			case 1:
+				$interes = 1.69;
+				break;
+			
+			case 2:
+				$interes = 1.50;
+				break;
+			case 3:
+				$interes = 1.50;
+				break;
+			case 4:
+				$interes = 1.8;
+				break;
+			case 5:
+				$interes = $otroPorcentaje;
+				break;
+		}
 
+		switch ($plazo) {
+					case 12:
+						$plazoAnnos = "1 año";
+						break;
+					case 24:
+						$plazoAnnos = "2 años";
+						break;
+					case 36:
+						$plazoAnnos = "3 años";
+						break;
+					case 48:
+						$plazoAnnos = "4 años";
+						break;
+					case 60:
+						$plazoAnnos = "5 años";
+						break;
+				}
 
+		$cuota = $valor*$interes*1+$interes+$plazo / 1+$interes*$plazo-1;
 
+		$salarioFinal = $salario * 0.30;
 
+		$interesAnual = $cuota*$interes*$plazo;
 
+		$totalPagar = $interesAnual + $valor;
 
+		if($cuota > $salarioFinal){
+			$estadoCredito = "Rechazado";
+		}else{
+			$estadoCredito = "Aprobado";
+		}
 
+		echo "<br><br><h2 class='espacio-arriba font-open-sans text-center font-mediana'>" .
+			$generoFinal . $nombre . " identificado con el número de indentificación " . $cedula . " Le comunicamos que segun su ingreso mensual $" . $salario . " Su credito fue " .
+			$estadoCredito;
 
+			if($estadoCredito == "Rechazado"){
+				echo " Debido a que su deducción mensual sería $" . $salarioFinal  . " y " .
+				"supera el 30 % permitido por deducción" ."</h2>";
+			}else{
+				echo " y su resumen del crédito queda de la siguiente manera: <br><br>".
+				"Detalle Credito: <br><br>" .
+				"Monto: $" . $valor . "<br>" .
+				"Duración Crédito: " . $plazoAnnos . "<br>" .
+				"Intereses Anuales: $" . $interesAnual . "<br>".
+				"Periodo de pago: " . $plazo ." meses <br>".
+				"Cuota Mensual: $" . $cuota . "<br>" .
+				"Total Pagar: $" . $totalPagar . "<br>" .
+				"Cantidad Cuotas: " .$plazo . "<br>";
+			}
 
+	?>
 
-				echo $generoFinal ." " . $nombre . " Le comunicamos que segun su ingreso mensual
-				se le consede / o no se le consede (El prestamo se da si la cuota no pasa del 30% del salario que gana, cuando es no se dar motivo, cuando es si mostrar:)
-
-				Resumen del credito:
-				Monto
-				Duracion credito (En años)
-				Intereses anuales
-				Periodo de pago (Meses)
-				Cuota mensual 
-				Total a pagar
-				Cantidad de cuotas";	
-
-
- 		?>
 	</div>
 
 </body>
